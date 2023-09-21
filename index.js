cdd1229bc5502506dc097d2d4d849887466d705c
mmwandera <mmwandera74@gmail.com>
2023-09-21 23:38:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert("I'm graduating tomorrow. Look Mama I made it!");
+}
+
+input.addEventListener('click', clickAlert);
 }
