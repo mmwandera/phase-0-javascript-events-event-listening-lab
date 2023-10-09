227095e5b81018cd7d2eb6769f94b00676d85425
mmwandera <mmwandera74@gmail.com>
2023-10-09 13:05:56 +0300

    Formatted code and added comments

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// binds an event listener in addingEventListener()
 function addingEventListener() {
-    const input = document.getElementById('input');
+  const input = document.getElementById('input');
 
-function clickAlert() {
-  alert("I'm graduating tomorrow. Look Mama I made it!");
-}
+  function clickAlert() {
+    alert("I'm graduating tomorrow. Look Mama I made it!");
+  }
 
-input.addEventListener('click', clickAlert);
+  input.addEventListener('click', clickAlert);
 }
